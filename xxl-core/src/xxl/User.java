943bba5de4b2ee6992abed245c712d573abcf1f4
Pedro Ruano Silveira <pruanosilveira@gmail.com>
2023-10-24 06:27:06 +0100

    user

--- xxl-core/src/xxl/User.java
+++ xxl-core/src/xxl/User.java
@@ -1,4 +1,26 @@
 package xxl;
 
-public class User {
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class User implements Comparable<User>, Serializable {
+
+    private String _name;
+    private List<Spreadsheet> _spreadsheets = new ArrayList<>();
+
+    public User(String name) { _name = name; }
+
+    public String getName() { return _name; }
+
+    public void addSpreadsheet(Spreadsheet spreadsheet) {
+        _spreadsheets.add(spreadsheet);
+    }
+
+    @Override
+    public int compareTo(User user) {
+        return _name.compareTo(user.getName());
+    }
 }
