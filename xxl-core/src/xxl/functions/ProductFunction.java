61b84777e9db28e4200ccdd3c1403ae681f6d236
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2023-10-26 14:26:55 +0100

    Product function inverted arg check fixed

--- xxl-core/src/xxl/functions/ProductFunction.java
+++ xxl-core/src/xxl/functions/ProductFunction.java
@@ -11,7 +11,7 @@ public class ProductFunction extends RangeFunctionStrategy implements Serializab
     public Literal<?> executeOperation(List<Literal<?>> operands) {
         int result = 1;
         for (Literal<?> operand: operands) {
-            if (!invalidIntegerArgument(operand)) return null;
+            if (invalidIntegerArgument(operand)) return null;
             result *= (int) operand.getValue();
         }
         if (!operands.isEmpty())
