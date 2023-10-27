6723e5f663961e3fbf78c6b832a18ecf7c125be5
Pedro Ruano Silveira <pruanosilveira@gmail.com>
2023-10-27 05:37:26 +0100

    all tests passed

--- xxl-core/src/xxl/search/SearchValue.java
+++ xxl-core/src/xxl/search/SearchValue.java
@@ -1,8 +1,7 @@
 package xxl.search;
 
-
-import xxl.content.Content;
-import xxl.content.IntegerLiteral;
+import xxl.Range;
+import xxl.content.*;
 import xxl.storage.Storage;
 import xxl.visitor.ReadContent;
 
@@ -12,11 +11,17 @@ import java.util.Map;
 public class SearchValue implements SearchPredicate {
     @Override
     public boolean test(String argument, Content sheetCellContent, Storage data) {
-        IntegerLiteral arg = new IntegerLiteral(argument);
-        ReadContent visitor2 = new ReadContent();
-        sheetCellContent.requestContent(visitor2, data);
-        if (visitor2.readContent().isIntegerLiteral())
-            return arg.getValue() == visitor2.readContent().getValue();
+        ContentBuilder contentBuilder = new ContentBuilder();
+        Content content = contentBuilder.build(argument);
+        ReadContent visitor = new ReadContent();
+        sheetCellContent.requestContent(visitor, data);
+        if (content.isIntegerLiteral() && visitor.readContent().isIntegerLiteral()) {
+            IntegerLiteral arg  = new IntegerLiteral(argument);
+            return arg.getValue() == visitor.readContent().getValue();
+        } else if (content.isStringLiteral() && visitor.readContent().isStringLiteral()) {
+            StringLiteral arg = new StringLiteral(argument);
+            return arg.getValue().equals(visitor.readContent().getValue());
+        }
         return false;
     }
 
@@ -24,4 +29,5 @@ public class SearchValue implements SearchPredicate {
     public void sort(List<Map.Entry<Integer, Content>> matchedContent) {
         // doesn't need sorting
     }
+
 }
